ea928eba2918e29fa947b6f4d1e8550628e224d2
julietvt <freshitpt@gmail.com>
2021-11-14 11:18:04 +0200

    add action types for user component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
 import ACTION_TYPES from './actionsType';
 
+//counter
+
 export const dec = () => ({ type: ACTION_TYPES.DECREMENT });
 
 export const inc = () => ({ type: ACTION_TYPES.INCREMENT });
@@ -8,3 +10,26 @@ export const setStep = (newStep) => ({
   type: ACTION_TYPES.SET_STEP,
   value: newStep,
 });
+
+//user
+
+export const createUser = (userData) => {
+  return {
+    type: ACTION_TYPES.CREATE_USER,
+    userData: userData,
+  };
+};
+
+export const deleteUser = (id) => {
+  return {
+    type: ACTION_TYPES.DELETE_USER,
+    id: id,
+  };
+};
+
+export const updateUser = (newData) => {
+  return {
+    type: ACTION_TYPES.UPDATE_USER,
+    newData: newData,
+  };
+};
