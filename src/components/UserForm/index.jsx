9529ce6719304928b55e320c79d362f34cb18b23
julietvt <freshitpt@gmail.com>
2021-11-14 11:27:21 +0200

    component user form was updated and conected

--- src/components/UserForm/index.jsx
+++ src/components/UserForm/index.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
-import {Formik, Form, Filed} from 'formik';
+import {Formik, Form, Field} from 'formik';
+import { connect } from 'react-redux';
+import { createUser } from '../../actions';
 
 const UserForm = () => {
     const initialUserValues = {
         nameUser: '',
         emailUser: '',
-        isMaleUser: true,
     };
     const submitHandler = (values, formikBag) => {
+        createUserAction(values);
         formikBag.resetForm();
     };
     return(
         <Formik initialValues={initialUserValues} onSubmit={submitHandler}>
             {formikProps => {
                 <Form>
-                    <Filed name="name" />
-                    <Filed name="emailUser" />
-                    <Filed name="isMaleUser" />
-                    <button type='submit' />
+                    <Field name="name" />
+                    <Field name="emailUser" />
+                    <button type='submit'>Add user</button>
                 </Form>
             }}
         </Formik> 
     );
 }
 
-export default UserForm;
+ const mapDispatchToProps = dispatch => {
+     return{
+         createUserAction: data => {
+             dispatch(createUser(data));
+         }
+     };
+ };
+
+export default connect(mapStateToProps, mapDispatchToProps) (UserForm);
